39ce395f440b911eb4c3df50c322181590d0aa29
DevendTech <pablolucas@Pablos-MacBook-Pro.local>
2024-12-16 17:15:56 -0300

    mudança das empresas do carrousel e primeiro media query feito

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 let copy = document.querySelector(".section-clients__logo-slide").cloneNode(true)
 document.querySelector('.section-clients').appendChild(copy)
+
+const hamburguer = document.querySelector(".hamburguer");
+const nav = document.querySelector("nav")
+const ul = document.querySelector("nav ul")
+
+hamburguer.addEventListener("click", ()=>{
+  ul.classList.toggle("active")
+})
