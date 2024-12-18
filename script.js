71c6ecfe71695969cde962ea2abc9849e4105dd3
DevendTech <pablolucas@Pablos-MacBook-Pro.local>
2024-12-18 08:57:40 -0300

    efeito ao scrollar

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const body = document.querySelector('body')
+
+body.classList.add('js')
+
 let copy = document.querySelector(".section-clients__logo-slide").cloneNode(true)
 document.querySelector('.section-clients').appendChild(copy)
 
@@ -8,3 +12,23 @@ const ul = document.querySelector("nav ul")
 hamburguer.addEventListener("click", ()=>{
   ul.classList.toggle("active")
 })
+
+/* Scroll animation */
+
+const sections = document.querySelectorAll('.js-scroll')
+if(sections.length){
+
+  const windowMetade = window.innerHeight * 0.6
+  function scrollAnimation(){
+    sections.forEach((section) =>{
+      const sectionTop = section.getBoundingClientRect().top - windowMetade
+      if(sectionTop < 0){
+        section.classList.add("ativo")
+      }
+  
+    })
+  }
+  scrollAnimation()
+  window.addEventListener('scroll',scrollAnimation)
+}
+
